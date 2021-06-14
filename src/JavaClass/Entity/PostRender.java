a76425c4c14b1d3256b3ead35480854b9fd72eff
Nghiatran2603 <82387132+Nghiatran2603@users.noreply.github.com>
2021-06-14 13:06:31 +0700

    end

--- src/JavaClass/Entity/PostRender.java
+++ src/JavaClass/Entity/PostRender.java
@@ -6,6 +6,5 @@ import javafx.scene.canvas.GraphicsContext;
 public class PostRender {
     public static void Update(GraphicsContext gc){
         gc.drawImage(Assets.Instance.zone1[53],160,5,231,66);
-
     }
 }
